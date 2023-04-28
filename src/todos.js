710bc9a2a522563c485b2ed4794454d44e23c2e0
Mohammad Alfares <42076538+mohammad0alfares@users.noreply.github.com>
2023-04-28 22:59:33 +0200

    a

--- src/todos.js
+++ src/todos.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>adding box ---- link githib fork </h1>
+      <h1>adding box --c-- link githib fork </h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
